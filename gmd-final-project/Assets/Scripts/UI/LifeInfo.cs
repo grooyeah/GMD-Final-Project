cf2abfce72d4c08a77b578d5b94478763e6b9f7d
Gruia Pana <gruiapana99@gmail.com>
2024-03-25 16:53:55 +0100

    Add levels and UI

---
+++ gmd-final-project/Assets/Scripts/UI/LifeInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LifeInfo : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _lifeText;
+
+    private void Awake()
+    {
+        if(_lifeText != null)
+        {
+            _lifeText.text = "❤❤❤";
+        }
+    }
+}
---
+++ gmd-final-project/Assets/Scripts/UI/ScoreInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreInfo : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _scoreText;
+
+    private void Awake()
+    {
+        if (_scoreText != null)
+        {
+            _scoreText.text = "00000";
+        }
+    }
+}
